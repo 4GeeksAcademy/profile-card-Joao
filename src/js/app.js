ca26e088b293b1605b0aef8d5183dccf390e810d
GJoao24 <122391147+GJoao24@users.noreply.github.com>
2025-06-25 15:55:13 +0000

    Perfil

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1 >${variables.name + " " + variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -64,10 +64,10 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
+    name: "Lucy",
+    lastName: "Gomez",
+    role: "Web Developer",
+    country: "Miami,USA",
     city: null
   };
   render(window.variables); // render the card for the first time
